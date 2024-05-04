f89f9691acf2385a29b1e54ab0e76b49717b3e41
Sandi Fox <sandimixyz@gmail.com>
2024-05-04 09:36:47 +0700

    Portfolio

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -46,7 +46,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
+          I'm a Full Stack Software Engineer with experience in Website,
           Mobile, and Software development. Check out my projects and skills.
         </motion.p>
         <motion.a
